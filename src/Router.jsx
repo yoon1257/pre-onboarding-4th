039542eb9bc99072099637d18280e8e30929a8fd
yuna <yureve5772@gmail.com>
2022-10-15 17:28:52 +0900

    Modify: 확장자변경

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../src/pages/Main';
 
-const Router = (props) => {
+const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
