abdc2e9787b2040de5a1066738b16652c77a4e75
yuna <108171986+yoon1257@users.noreply.github.com>
2022-10-17 23:26:31 +0900

    Feature/mockdata (#2)
    * Add: MockData 병원리스트 만듬
    
    * Add: 지도 데이터 연동

--- src/components/list/map.jsx
+++ src/components/list/map.jsx
@@ -3,32 +3,25 @@ import styled from 'styled-components';
 
 const { kakao } = window;
 
-const Main = styled.div`
-  // 원하는 css 스타일링
-  width: 100%;
-  height: 600px;
-  .mapDetail {
-    // 원하는 css 스타일링
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const Map = ({ list }) => {
+const Map = ({ data }) => {
   useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(37.504897, 127.04961),
-      level: 3,
-      // center: new kakao.maps.LatLng(37.504897, 127.04961),
-      // level: 6,
+    const mapContainer = document.getElementById('map');
+    const mapOptions = {
+      center: new kakao.maps.LatLng(37.5001154, 127.041344),
+      level: 6,
     };
-    const kakaoMap = new kakao.maps.Map(container, options);
-    const marker = new kakao.maps.Marker({
-      position: new kakao.maps.LatLng(37.504897, 127.04961),
-    });
-    marker.setMap(kakaoMap);
-  }, [list]);
+    const kakaoMap = new kakao.maps.Map(mapContainer, mapOptions);
+    const zoomControl = new kakao.maps.ZoomControl();
+    kakaoMap.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    data &&
+      data.forEach((el) => {
+        const marker = new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(el.lat, el.lng),
+        });
+        marker.setMap(kakaoMap);
+      });
+  }, [data]);
 
   return (
     <Main>
@@ -36,5 +29,12 @@ const Map = ({ list }) => {
     </Main>
   );
 };
-
+const Main = styled.div`
+  width: 100%;
+  height: 600px;
+  .mapDetail {
+    width: 100%;
+    height: 100%;
+  }
+`;
 export default Map;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 import List from '../components/list/List';
 import Map from '../components/list/map';
 const Main = () => {
+  const [showData, setShowData] = useState();
+
+  useEffect(() => {
+    axios
+      .get('./data/hospital.json')
+      .then((res) => {
+        setShowData(res.data);
+      })
+      .catch((err) => {
+        console.log('에러에러', err);
+      });
+  }, []);
   return (
-    <div>
-      <Map />
+    <MainBlock>
+      <Map data={showData} />
       <List />
-    </div>
+    </MainBlock>
   );
 };
+
+const MainBlock = styled.div``;
 
 export default Main;
